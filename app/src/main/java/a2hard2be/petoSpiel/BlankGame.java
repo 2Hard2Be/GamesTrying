cf2e94c4d9053a42b9c7d89cedd9504377c6f0c6
2Hard2Be <luisrivas.power@gmail.com>
2018-05-17 21:15:10 -0600

    Llego el pedido

--- app/src/main/java/a2hard2be/petoSpiel/BlankGame.java
+++ app/src/main/java/a2hard2be/petoSpiel/BlankGame.java
@@ -10,6 +10,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 
+import android.graphics.RectF;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
@@ -51,6 +52,16 @@ LadrillosView ladrillosView;
 
         Paddle paddle;
         Ball ball;
+        Brick[] bricks = new Brick[200];
+        int numBricks = 0;
+        SoundPool soundPool;
+        int beep1ID = -1;
+        int beep2ID = -1;
+        int beep3ID = -1;
+        int loseLifeID = -1;
+        int explodeID = -1;
+        int score = 0;
+        int lives = 3;
 
         public LadrillosView(Context context) {
             super(context);
@@ -63,12 +74,48 @@ LadrillosView ladrillosView;
             screenY = size.y;
             paddle = new Paddle(screenX, screenY);
             ball = new Ball(screenX,screenY);
+            soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC,0);
+
+            try{
+                AssetManager assetManager = context.getAssets();
+                AssetFileDescriptor descriptor;
+
+                descriptor = assetManager.openFd("beep1.ogg");
+                beep1ID = soundPool.load(descriptor, 0);
+
+                descriptor = assetManager.openFd("beep2.ogg");
+                beep2ID = soundPool.load(descriptor, 0);
+
+                descriptor = assetManager.openFd("beep3.ogg");
+                beep3ID = soundPool.load(descriptor, 0);
+
+                descriptor = assetManager.openFd("loseLife.ogg");
+                loseLifeID = soundPool.load(descriptor, 0);
+
+                descriptor = assetManager.openFd("explode.ogg");
+                explodeID = soundPool.load(descriptor, 0);
+
+            }catch(IOException e){
+
+                Log.e("error", "failed to load sound files");
+            }
             createBricksAndRestart();
         }
-
         public void createBricksAndRestart(){
 
             ball.reset(screenX,screenY);
+            int brickWidth = screenX / 8;
+            int brickHeight = screenY / 10;
+            numBricks = 0;
+
+            for(int column = 0; column < 8; column ++ ){
+                for(int row = 0; row < 3; row ++ ){
+                    bricks[numBricks] = new Brick(row, column, brickWidth, brickHeight);
+                    numBricks ++;
+                }
+            }
+            score = 0;
+            lives = 3;
         }
 
         @Override
@@ -90,6 +137,59 @@ LadrillosView ladrillosView;
         public void update() {
 
             paddle.update(fps);
+            for(int i = 0; i < numBricks; i++){
+
+                if (bricks[i].getVisibility()){
+
+                    if(RectF.intersects(bricks[i].getRect(),ball.getRect())) {
+                        bricks[i].setInvisible();
+                        ball.reverseYVelocity();
+                        score = score + 10;
+                        soundPool.play(explodeID, 1, 1, 0, 0, 1);
+                    }
+                }
+            }
+            if(RectF.intersects(paddle.getRect(),ball.getRect())) {
+                ball.setRandomXVelocity();
+                ball.reverseYVelocity();
+                ball.clearObstacleY(paddle.getRect().top - 2);
+                soundPool.play(beep1ID, 1, 1, 0, 0, 1);
+            }
+            if(ball.getRect().bottom > screenY){
+                ball.reverseYVelocity();
+                ball.clearObstacleY(screenY - 2);
+
+                // Lose a life
+                lives --;
+                soundPool.play(loseLifeID, 1, 1, 0, 0, 1);
+
+                if(lives == 0){
+                    paused = true;
+                    createBricksAndRestart();
+                }
+
+            }
+            if(ball.getRect().top < 0){
+                ball.reverseYVelocity();
+                ball.clearObstacleY(12);
+                soundPool.play(beep2ID, 1, 1, 0, 0, 1);
+            }
+            if(ball.getRect().left < 0){
+                ball.reverseXVelocity();
+                ball.clearObstacleX(2);
+                soundPool.play(beep3ID, 1, 1, 0, 0, 1);
+            }
+            if(ball.getRect().right > screenX - 10){
+                ball.reverseXVelocity();
+                ball.clearObstacleX(screenX - 22);
+                soundPool.play(beep3ID, 1, 1, 0, 0, 1);
+            }
+            if(score == numBricks * 10){
+                paused = true;
+                createBricksAndRestart();
+            }
+
+
             ball.update(fps);
 
         }
@@ -104,7 +204,30 @@ LadrillosView ladrillosView;
 
                 canvas.drawRect(paddle.getRect(), paint);
                 canvas.drawRect(ball.getRect(), paint);
+                paint.setColor(Color.argb(255,  249, 129, 0));
+                for(int i = 0; i < numBricks; i++){
+                    if(bricks[i].getVisibility()) {
+                        canvas.drawRect(bricks[i].getRect(), paint);
+                    }
+                }
 
+                paint.setColor(Color.argb(255,  255, 255, 255));
+
+                // Draw the score
+                paint.setTextSize(40);
+                canvas.drawText("Score: " + score + "   Lives: " + lives, 10,50, paint);
+
+
+                if(score == numBricks * 10){
+                    paint.setTextSize(90);
+                    canvas.drawText("YOU HAVE WON!", 10,screenY/2, paint);
+                }
+
+
+                if(lives <= 0){
+                    paint.setTextSize(90);
+                    canvas.drawText("YOU HAVE LOST!", 10,screenY/2, paint);
+                }
                 ourHolder.unlockCanvasAndPost(canvas);
 
             }
